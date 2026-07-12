423a53f1cf95990e7bbe4a489efbef97d2756f59
novakvova <novakvova@gmail.com>
2026-07-12 13:25:17 +0300

    Working is good project

---
+++ README.md
@@ -0,0 +1,11 @@
+# Створення проекту React App js - на JavaScript
+```
+node -v
+npm -v
+
+npx create-react-app my-app
+cd my-app
+npm start
+```
+
+
---
+++ my-app/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <h1 style={{textAlign: "center"}}>Привіт команда :)</h1>
+      
+    </>
+  );
+}
+
+export default App;
