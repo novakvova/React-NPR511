ed43ff47f5ff37735e12616ec634ec1b1db24584
novakvova <novakvova@gmail.com>
2026-08-23 12:50:33 +0300

    QR code is good

--- my-boot/src/pages/LoginPage.js
+++ my-boot/src/pages/LoginPage.js
@@ -3,6 +3,8 @@ const LoginPage = () => {
     return (
         <>
             <h1 className="text-center">Вхід на сайт</h1>
+
+
         </>
     )
 }
--- my-boot/src/pages/QRPage.js
+++ my-boot/src/pages/QRPage.js
@@ -1,11 +1,36 @@
 import QRCode from "react-qr-code";
+import {useState} from "react";
 
 const QRPage = () => {
+    //useState - це спеціальний хук для зберігання інформації
+    //Якщо дані змінюються, відбувається render компонента
+    //qrCode - це змінна, яка зберігає значення
+    //setQrCode - це фукнція, яка онолвяє змінну qrCode і робить render
+    const [qrCode, setQrCode] = useState("https://makeup.com.ua/ua/product/9811/");
+
+    // console.log("qrCode", qrCode);
+    const onChangeInput = (e) => {
+        // e - це сам input - у якому відбуваються зміниї
+        //Подія яка спрацьовує коли дані у input змінюються
+        setQrCode(e.target.value); //Суди передаємо значення, яке є input на даний момент
+    }
+
     return (
-        <>
-            <h1 className={"text-center"}>Генерація QR кодів</h1>
-            <QRCode value="https://kherson.itstep.org/" />
-        </>
+        <div className="container">
+            <div className={"col-md-8 offset-md-2 mt-2"}>
+                <h1 className={"text-center"}>Генерація QR кодів</h1>
+                <div className="mb-3">
+                    <label htmlFor="myInputQR" className="form-label">Вкажіть посилання на сайт</label>
+                    <input type="text"
+                           className="form-control"
+                           id="myInputQR"
+                           value = {qrCode}
+                           onChange={onChangeInput}
+                    />
+                </div>
+                <QRCode value={qrCode}/>
+            </div>
+        </div>
     )
 }
 
