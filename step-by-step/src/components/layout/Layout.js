d2b465562919c967448c03afa4a947225ba4a25e
novakvova <novakvova@gmail.com>
2026-09-13 13:07:01 +0300

    Working is good LoginPage

--- step-by-step/src/components/layout/Layout.js
+++ step-by-step/src/components/layout/Layout.js
@@ -5,7 +5,9 @@ const Layout = () => {
     return (
         <>
             <Navbar/>
-            <Outlet/>
+            <div className={"container"}>
+                <Outlet/>
+            </div>
         </>
     )
 }
--- step-by-step/src/pages/login/LoginPage.js
+++ step-by-step/src/pages/login/LoginPage.js
@@ -1,8 +1,55 @@
+//Значення, які будуть в input на початку
+import {loginSchema} from "./validation";
+import {zodResolver} from "@hookform/resolvers/zod";
+import {useForm} from "react-hook-form";
+
+const defaultValues = {
+    email: "",
+    password: ""
+}
 
 const LoginPage = () => {
+
+    //Ми створюємо форму де вказуємо систему валіації
+
+    const {
+        register, //Для використання value та onChange у input
+        handleSubmit, //Для виклику валації форми при роботі
+        formState: {errors}, //Що не вірно вказано у формі
+    } = useForm({
+        resolver: zodResolver(loginSchema),
+        defaultValue: defaultValues,
+    });
+
+    //Буде викликатися коли усе вказано вірно у формі входу
+    const onMySubmit = (data) => {
+        console.log("Дані форми входу", data);
+    }
+
     return (
         <>
-            <h1 className={"text-center"}>Login</h1>
+            <h1 className={"text-center"}>Вхід на сайт</h1>
+            <form className={"col-md-6 offset-md-3"} onSubmit={handleSubmit(onMySubmit)}>
+                <div className="mb-3">
+                    <label htmlFor="email" className="form-label">Електронна пошта</label>
+                    <input type="email"
+                           className="form-control"
+                           id="email"
+                           {...register("email")}
+                           aria-describedby="emailHelp"/>
+                    {errors.email && <span className={"text-danger"}>{errors.email.message}</span>}
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="password" className="form-label">Пароль</label>
+                    <input type="password"
+                           className="form-control"
+                           {...register("password")}
+                           id="password"
+                           aria-describedby="emailHelp"/>
+                    {errors.password && <span className={"text-danger"}>{errors.password.message}</span>}
+                </div>
+                <input type={"submit"} className={"btn btn-danger"} value={"Вхід"}/>
+            </form>
         </>
     )
 }
